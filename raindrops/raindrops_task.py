7191ba7f18222319ac19e5e63fd15c38ad172afa
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-01 13:46:49 +0200

    Added a Raindrops task

---
+++ raindrops/raindrops_task.py
@@ -0,0 +1,13 @@
+# Task from https://exercism.org/tracks/python/exercises/raindrops
+
+def convert(number):
+    result = ""
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+    if not result:
+        return str(number)
+    return result
