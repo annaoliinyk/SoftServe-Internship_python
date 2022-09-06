ab795a1076a41bb61700e7ea3934b10e42c06a39
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-06 10:25:55 +0200

    Added a main method

--- leap/leap_task.py
+++ leap/leap_task.py
@@ -2,3 +2,13 @@
 
 def leap_year(year):
     return year % 4 == 0 and not (year % 100 == 0 and year % 400 != 0)
+
+
+def main():
+    print(leap_year(1996))
+    print(leap_year(2000))
+    print(leap_year(1997))
+
+
+if __name__ == "__main__":
+    main()
