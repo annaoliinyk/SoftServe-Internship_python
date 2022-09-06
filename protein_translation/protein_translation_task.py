6780f60255876ebfc7e50038db31d62e1ba1f8e3
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-06 10:58:28 +0200

    Moved constants outside function

--- protein_translation/protein_translation_task.py
+++ protein_translation/protein_translation_task.py
@@ -1,33 +1,36 @@
 # Task from https://exercism.org/tracks/python/exercises/protein-translation
 
+switcher = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan"
+}
+stop_codons = ["UAA", "UAG", "UGA"]
+
+
 def proteins(strand):
-    codons_list = get_codors_list(strand)
+    codons_list = get_codons_list(strand)
     proteins_list = []
     for codon in codons_list:
-        if codon in ["UAA", "UAG", "UGA"]:
+        if codon in stop_codons:
             return proteins_list
-        switcher = {
-            "AUG": "Methionine",
-            "UUU": "Phenylalanine",
-            "UUC": "Phenylalanine",
-            "UUA": "Leucine",
-            "UUG": "Leucine",
-            "UCU": "Serine",
-            "UCC": "Serine",
-            "UCA": "Serine",
-            "UCG": "Serine",
-            "UAU": "Tyrosine",
-            "UAC": "Tyrosine",
-            "UGU": "Cysteine",
-            "UGC": "Cysteine",
-            "UGG": "Tryptophan"
-        }
         protein = switcher.get(codon)
         proteins_list.append(protein)
     return proteins_list
 
 
-def get_codors_list(strand):
+def get_codons_list(strand):
     codons_list = []
     for i in range(0, len(strand), 3):
         codons_list.append(strand[i:i + 3])
