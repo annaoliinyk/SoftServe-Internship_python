d51f2eecefc74d7fc56481b4c317acfada1c4bc3
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-06 09:38:55 +0200

    Made a dict constant, added main() method

--- resistor_color/resistor_color_task.py
+++ resistor_color/resistor_color_task.py
@@ -1,20 +1,35 @@
 # Task from https://exercism.org/tracks/python/exercises/resistor-color
 
+switch = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+colors_list = list(switch.keys())
+
+
 def color_code(color):
-    switch = {
-        "black": 0,
-        "brown": 1,
-        "red": 2,
-        "orange": 3,
-        "yellow": 4,
-        "green": 5,
-        "blue": 6,
-        "violet": 7,
-        "grey": 8,
-        "white": 9
-    }
     return switch.get(color)
 
 
 def colors():
-    return ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+    return colors_list
+
+
+def main():
+    print(color_code("black"))
+    print(color_code("white"))
+    print(color_code("orange"))
+    print(colors())
+
+
+if __name__ == "__main__":
+    main()
