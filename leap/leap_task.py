e97bf6fb87e950edefb658f864e44dbd3a0333bc
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-06 10:22:50 +0200

    Re-written without if-else structure

--- leap/leap_task.py
+++ leap/leap_task.py
@@ -1,8 +1,4 @@
 # Task from https://exercism.org/tracks/python/exercises/leap
 
 def leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0 and year % 400 != 0:
-            return False
-        return True
-    return False
+    return year % 4 == 0 and not (year % 100 == 0 and year % 400 != 0)
