ba8435a69cd4b227e0fa78c752c70a1d721585b5
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-07 12:10:01 +0200

    Added logging messages, made dict a constant

--- resistor_color_duo/resistor_color_duo_task.py
+++ resistor_color_duo/resistor_color_duo_task.py
@@ -1,28 +1,49 @@
 # Task from https://exercism.org/tracks/python/exercises/resistor-color-duo
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+switch = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+
 def value(colors):
-    if not colors:
-        return None
-    elif len(colors) == 1:
-        result = color_value(colors[0].lower())
+    if type(colors) == list:
+        if not colors:
+            return None
+        first_color = colors[0].lower()
+        try:
+            if len(colors) == 1:
+                result = switch.get(first_color)
+            else:
+                second_color = colors[1].lower()
+                # to get two digits result, multiplying first digit by 10:
+                result = switch.get(first_color) * 10 + switch.get(second_color)
+            return result
+        except:
+            logging.error("Invalid color provided")
     else:
-        result = color_value(colors[0].lower())
-        result *= 10
-        result += color_value(colors[1].lower())
-    return result
+        logging.error("Invalid input - should be a list")
 
 
-def color_value(color):
-    switcher = {
-        "black": 0,
-        "brown": 1,
-        "red": 2,
-        "orange": 3,
-        "yellow": 4,
-        "green": 5,
-        "blue": 6,
-        "violet": 7,
-        "grey": 8,
-        "white": 9
-    }
-    return switcher.get(color)
+def main():
+    logging.debug(value(["red"]))
+    logging.debug(value("blue"))
+    logging.debug(value(["green", "white"]))
+    logging.debug(value(["green", "while"]))
+    logging.debug(value(["green", "white", "red"]))
+
+
+if __name__ == "__main__":
+    main()
