d9712f9a63ca9f477932cabd5314fa24f7069177
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-08 16:45:19 +0200

    Added main(), simplified logic, added logging messages

--- matching_brackets/matching_brackets_task.py
+++ matching_brackets/matching_brackets_task.py
@@ -1,67 +1,33 @@
 # Task from https://exercism.org/tracks/python/exercises/matching-brackets
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 def is_paired(input_string):
-    number_of_braces_dict = {"[": input_string.count("["),
-                             "]": input_string.count("]"),
-                             "{": input_string.count("{"),
-                             "}": input_string.count("}"),
-                             "(": input_string.count("("),
-                             ")": input_string.count(")"),
-                             }
-    return check_number_of_brackets(number_of_braces_dict) and check_the_order(input_string)
+    brackets_counter, braces_counter, parentheses_counter = get_brackets_counters(input_string)
+    return brackets_counter == 0 and braces_counter == 0 and parentheses_counter == 0
 
 
-def check_number_of_brackets(number_of_braces_dict):
-    number_of_brackets_is_equal = number_of_braces_dict["["] == number_of_braces_dict["]"]
-    number_of_braces_is_equal = number_of_braces_dict["{"] == number_of_braces_dict["}"]
-    number_of_parentheses_is_equal = number_of_braces_dict["("] == number_of_braces_dict[")"]
-    return number_of_brackets_is_equal and number_of_braces_is_equal and number_of_parentheses_is_equal
+# the method is used to check if number of beginning and ending brackets is same (counter == 0 if yes)
+def get_brackets_counters(input_string):
+    # declare counters for [], {} and () accordingly;
+    # count number of begin brackets and subtract number of ending brackets
+    brackets_counter = input_string.count("[") - input_string.count("]")
+    braces_counter = input_string.count("{") - input_string.count("}")
+    parentheses_counter = input_string.count("(") - input_string.count(")")
+    return brackets_counter, braces_counter, parentheses_counter
 
 
-def check_the_order(input_string):
-    input_string = input_string.replace(" ", "")
-    input_string_as_list = list(input_string)
-    check_brackets_order, check_braces_order, check_parentheses_order = True, True, True
-    begin_brackets, begin_braces, begin_parentheses = 0, 0, 0
-    end_brackets, end_braces, end_parentheses = len(input_string), len(input_string), len(input_string)
-    # check if indexes of begin brackets match the indexes of end brackets for all pairs: [], {}, ()
+# TODO: add method checking if nested correctly - if order is correct
 
-    if "[" in input_string_as_list:
-        begin_brackets = input_string_as_list.index("[")
-        end_brackets = input_string_as_list.index("]")
-        check_brackets_order = begin_brackets < end_brackets
+def main():
+    logging.info(8)
+    logging.info(is_paired("[](())"))  # True
+    logging.info(is_paired("[]("))  # False
+    logging.info(is_paired("[}{]"))  # False
+    logging.info(is_paired("[({]})"))  # False
 
-    if "{" in input_string_as_list:
-        begin_braces = input_string_as_list.index("{")
-        end_braces = input_string_as_list.index("}")
-        check_braces_order = begin_braces < end_braces
 
-    if "(" in input_string_as_list:
-        begin_parentheses = input_string_as_list.index("(")
-        end_parentheses = input_string_as_list.index(")")
-        check_parentheses_order = begin_parentheses < end_parentheses
-    # check if the order of brackets is correct:
-    if input_string.startswith("["):
-        if begin_braces != 0:
-            if end_brackets < end_braces and begin_brackets < begin_braces:
-                return False
-        if begin_parentheses != 0:
-            if end_brackets < end_parentheses and begin_brackets > begin_parentheses:
-                return False
-    elif input_string.startswith("{"):
-        if begin_brackets != 0:
-            if end_braces < end_brackets and begin_braces > begin_brackets:
-                return False
-        if begin_parentheses != 0:
-            if end_braces < end_parentheses and begin_braces < begin_parentheses:
-                return False
-
-    elif input_string.startswith("("):
-        if begin_braces != 0:
-            if end_parentheses < end_braces and begin_parentheses > begin_braces:
-                return False
-        if begin_brackets != 0:
-            if end_parentheses < end_brackets and begin_parentheses > begin_brackets:
-                return False
-    # return the result of both the indexes and order correctness
-    return check_brackets_order and check_braces_order and check_parentheses_order
+if __name__ == "__main__":
+    main()
