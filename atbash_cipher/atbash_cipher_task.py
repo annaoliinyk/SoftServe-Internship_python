f8568a262ced2581cd1a057b3e2162c5c62dd37c
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-08 19:56:46 +0200

    Renamed constants to be uppercase, changed logging from debug to info

--- atbash_cipher/atbash_cipher_task.py
+++ atbash_cipher/atbash_cipher_task.py
@@ -2,19 +2,19 @@
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-plain_str = "abcdefghijklm0123456789nopqrstuvwxyz"
-cipher_str = "zyxwvutsrqpon0123456789mlkjihgfedcba"
+PLAIN_STR = "abcdefghijklm0123456789nopqrstuvwxyz"
+CIPHER_STR = "zyxwvutsrqpon0123456789mlkjihgfedcba"
 
 
 def encode(plain_text):
     try:
         result = ""
         for symbol in plain_text.lower():
-            if len(result) % 5 == 0 and symbol in plain_str:
+            if len(result) % 5 == 0 and symbol in PLAIN_STR:
                 result += " "
-            result += if_symbol_in_str(plain_str, symbol)
+            result += if_symbol_in_str(PLAIN_STR, symbol)
         return result
     except:
         logging.error("Invalid input type")
@@ -24,7 +24,7 @@ def decode(ciphered_text):
     try:
         result = ""
         for symbol in ciphered_text.lower():
-            result += if_symbol_in_str(cipher_str, symbol)
+            result += if_symbol_in_str(CIPHER_STR, symbol)
         return result
     except:
         logging.error("Invalid input type")
@@ -33,19 +33,19 @@ def decode(ciphered_text):
 def if_symbol_in_str(str_to_check, symbol):
     tmp_result = ""
     if symbol in str_to_check:
-        index = cipher_str.index(symbol)
-        tmp_result += plain_str[index]
+        index = CIPHER_STR.index(symbol)
+        tmp_result += PLAIN_STR[index]
     return tmp_result
 
 
 def main():
-    logging.debug(encode("yes"))  # expected: "bvh"
-    logging.debug(encode("no"))  # expected: "ml"
-    logging.debug(encode("O M G"))  # expected: "lnt"
-    logging.debug(decode("vcvix rhn"))  # expected: "exercism"
-    logging.debug(decode("gvhgr mt123 gvhgr mt"))  # expected: "testing123testing"
-    logging.debug(encode(1))  # expected: error log
-    logging.debug(decode(["a", "b"]))  # expected: error log
+    logging.info(encode("yes"))  # expected: "bvh"
+    logging.info(encode("no"))  # expected: "ml"
+    logging.info(encode("O M G"))  # expected: "lnt"
+    logging.info(decode("vcvix rhn"))  # expected: "exercism"
+    logging.info(decode("gvhgr mt123 gvhgr mt"))  # expected: "testing123testing"
+    logging.info(encode(1))  # expected: error log
+    logging.info(decode(["a", "b"]))  # expected: error log
 
 
 if __name__ == "__main__":
