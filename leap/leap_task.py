0aeb882b5fd83235d4069c35b71ed37d11970cbc
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-08 14:51:29 +0200

    Changed print to logger in main(), added input args checking

--- leap/leap_task.py
+++ leap/leap_task.py
@@ -1,13 +1,21 @@
 # Task from https://exercism.org/tracks/python/exercises/leap
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 def leap_year(year):
-    return year % 4 == 0 and not (year % 100 == 0 and year % 400 != 0)
+    try:
+        return year % 4 == 0 and not (year % 100 == 0 and year % 400 != 0)
+    except:
+        logging.error("Invalid input")
 
 
 def main():
-    print(leap_year(1996))
-    print(leap_year(2000))
-    print(leap_year(1997))
+    logging.info(leap_year(1996))
+    logging.info(leap_year(2000))
+    logging.info(leap_year(1997))
+    logging.info(leap_year("1430"))
 
 
 if __name__ == "__main__":
