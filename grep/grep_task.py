ede4312c81f36b1ef344a7a6d33d9349a4fe5cd9
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-09 14:31:06 +0200

    Added files needed for program testing

--- grep/grep_task.py
+++ grep/grep_task.py
@@ -1,4 +1,8 @@
 # Task from https://exercism.org/tracks/python/exercises/grep
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 def grep(pattern, flags, files):
     if "-i" in flags:
@@ -9,12 +13,12 @@ def grep(pattern, flags, files):
         datafile = get_data_from_file(files[0])
         line_counter = 1
         for line in datafile:
-            result, non_matching_lines = get_matching_lines_single_file(line_counter, line, pattern, result, flags,
-                                                                        non_matching_lines)
+            result, non_matching_lines = get_matched_one_file(line_counter, line, pattern, result, flags,
+                                                              non_matching_lines)
             line_counter += 1
     elif len(files) > 1:
-        for i in range(len(files)):
-            datafile = get_data_from_file(files[i])
+        for file in files:
+            datafile = get_data_from_file(file)
             line_counter = 1
             for line in datafile:
                 line_to_match = line
@@ -22,33 +26,32 @@ def grep(pattern, flags, files):
                     line_to_match = line.lower()
                 if "-x" in flags:
                     if pattern + "\n" == line_to_match and "-n" in flags:
-                        result += files[i] + ":" + str(line_counter) + ":" + line
+                        result += file + ":" + str(line_counter) + ":" + line
                     elif pattern + "\n" == line_to_match:
-                        result += files[i] + ":" + line
+                        result += file + ":" + line
                     elif "-v" in flags:
-                        non_matching_lines += files[i] + ":" + line
+                        non_matching_lines += file + ":" + line
                 else:
                     if pattern in line_to_match and "-n" in flags:
-                        result += files[i] + ":" + str(line_counter) + ":" + line
+                        result += file + ":" + str(line_counter) + ":" + line
                     elif pattern in line_to_match:
-                        result += files[i] + ":" + line
+                        result += file + ":" + line
                     elif "-v" in flags:
                         if "-n" in flags:
-                            non_matching_lines += files[i] + ":" + str(line_counter) + ":" + line
+                            non_matching_lines += file + ":" + str(line_counter) + ":" + line
                         else:
-                            non_matching_lines += files[i] + ":" + line
+                            non_matching_lines += file + ":" + line
                 line_counter += 1
 
     if "-l" not in flags and "-v" not in flags:
         return result
     elif "-v" in flags:
         return non_matching_lines
-
     elif "-l" in flags:
         return return_filenames(files, result)
 
 
-def get_matching_lines_single_file(line_counter, line, pattern, result, flags, non_matching_lines=""):
+def get_matched_one_file(line_counter, line, pattern, result, flags, non_matching_lines=""):
     line_to_match = line
     if "-i" in flags:
         line_to_match = line.lower()
@@ -69,7 +72,6 @@ def get_matching_lines_single_file(line_counter, line, pattern, result, flags, n
                 non_matching_lines += str(line_counter) + ":" + line
             else:
                 non_matching_lines += line
-
     return result, non_matching_lines
 
 
@@ -86,7 +88,16 @@ def return_filenames(files, result):
         return files[0] + "\n"
     else:
         files_as_result = ""
-        for line in result.split("\n"):
+        result_as_list = result.split("\n")
+        for line in result_as_list:
             if line.split(":")[0] not in files_as_result:
                 files_as_result += line.split(":")[0] + "\n"
         return files_as_result
+
+
+def main():
+    logging.info(grep("Forbidden", "-l", ["paradise-lost.txt"]))  # "paradise-lost.txt\n"
+
+
+if __name__ == "__main__":
+    main()
