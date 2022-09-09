4270770b4923a4a1ea9f4df144f4e2697b231ef3
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-09 09:06:42 +0200

    Renamed constants to be uppercase, added logging and main()

--- protein_translation/protein_translation_task.py
+++ protein_translation/protein_translation_task.py
@@ -1,6 +1,10 @@
 # Task from https://exercism.org/tracks/python/exercises/protein-translation
 
-switcher = {
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+SWITCHER = {
     "AUG": "Methionine",
     "UUU": "Phenylalanine",
     "UUC": "Phenylalanine",
@@ -16,18 +20,21 @@ switcher = {
     "UGC": "Cysteine",
     "UGG": "Tryptophan"
 }
-stop_codons = ["UAA", "UAG", "UGA"]
+STOP_CODONS = ["UAA", "UAG", "UGA"]
 
 
-def proteins(strand):
-    codons_list = get_codons_list(strand)
-    proteins_list = []
-    for codon in codons_list:
-        if codon in stop_codons:
-            return proteins_list
-        protein = switcher.get(codon)
-        proteins_list.append(protein)
-    return proteins_list
+def proteins(strand: str):
+    try:
+        codons_list = get_codons_list(strand)
+        proteins_list = []
+        for codon in codons_list:
+            if codon in STOP_CODONS:
+                return proteins_list
+            protein = SWITCHER.get(codon)
+            proteins_list.append(protein)
+        return proteins_list
+    except:
+        logging.error("Invalid input")
 
 
 def get_codons_list(strand):
@@ -35,3 +42,14 @@ def get_codons_list(strand):
     for i in range(0, len(strand), 3):
         codons_list.append(strand[i:i + 3])
     return codons_list
+
+
+def main():
+    logging.info(proteins("UCG"))  # expected: ["Serine"]
+    logging.info(proteins("UAA"))  # expected: []
+    logging.info(proteins("UGGUAGUGG"))  # expected: ["Tryptophan"]
+    logging.info(proteins(["UGC", "UUG"]))  # expected: error logging message
+
+
+if __name__ == "__main__":
+    main()
