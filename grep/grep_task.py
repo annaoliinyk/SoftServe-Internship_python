9211182b29a786d024bc37e1cf95423c650fd040
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-09 15:00:13 +0200

    Added comments to make code understandable

--- grep/grep_task.py
+++ grep/grep_task.py
@@ -4,62 +4,85 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def grep(pattern, flags, files):
-    if "-i" in flags:
-        pattern = pattern.lower()
-    result, non_matching_lines = "", ""
-
-    if len(files) == 1:
-        datafile = get_data_from_file(files[0])
-        line_counter = 1
-        for line in datafile:
-            result, non_matching_lines = get_matched_one_file(line_counter, line, pattern, result, flags,
-                                                              non_matching_lines)
-            line_counter += 1
-    elif len(files) > 1:
-        for file in files:
-            datafile = get_data_from_file(file)
+def grep(pattern: str, flags: str, files: list):
+    try:
+        # if -i in flags - will ignore case while finding matched parts
+        if "-i" in flags:
+            pattern = pattern.lower()
+        result, non_matching_lines = "", ""
+        first_file = files[0]
+        # checking if one file provided, many files, or no files
+        if len(files) == 1:
+            # try to open file; if doesn't exist - program will stop and return None
+            try:
+                datafile = get_data_from_file(first_file)
+            except:
+                logging.error("Provided file does not exist")
+                return None
             line_counter = 1
             for line in datafile:
-                line_to_match = line
-                if "-i" in flags:
-                    line_to_match = line.lower()
-                if "-x" in flags:
-                    if pattern + "\n" == line_to_match and "-n" in flags:
-                        result += file + ":" + str(line_counter) + ":" + line
-                    elif pattern + "\n" == line_to_match:
-                        result += file + ":" + line
-                    elif "-v" in flags:
-                        non_matching_lines += file + ":" + line
-                else:
-                    if pattern in line_to_match and "-n" in flags:
-                        result += file + ":" + str(line_counter) + ":" + line
-                    elif pattern in line_to_match:
-                        result += file + ":" + line
-                    elif "-v" in flags:
-                        if "-n" in flags:
-                            non_matching_lines += file + ":" + str(line_counter) + ":" + line
-                        else:
-                            non_matching_lines += file + ":" + line
+                # iterate through lines to find matching lines per given flags
+                result, non_matching_lines = get_matched_one_file(line_counter, line, pattern, result, flags,
+                                                                  non_matching_lines)
                 line_counter += 1
+        elif len(files) > 1:
+            for file in files:
+                # try to open file; if doesn't exist - program will print error and file name
+                try:
+                    datafile = get_data_from_file(file)
+                    line_counter = 1
+                    for line in datafile:
+                        line_to_match = line
+                        if "-i" in flags:
+                            line_to_match = line.lower()
+                        # if -x in flags - only match entire lines, instead of lines that contain a match
+                        if "-x" in flags:
+                            if pattern + "\n" == line_to_match and "-n" in flags:
+                                result += file + ":" + str(line_counter) + ":" + line
+                            elif pattern + "\n" == line_to_match:
+                                result += file + ":" + line
+                            # if -v: collect all lines that fail to match the pattern
+                            elif "-v" in flags:
+                                non_matching_lines += file + ":" + line
+                        else:
+                            if pattern in line_to_match and "-n" in flags:
+                                result += file + ":" + str(line_counter) + ":" + line
+                            elif pattern in line_to_match:
+                                result += file + ":" + line
+                            # if -v: collect all lines that fail to match the pattern
+                            elif "-v" in flags:
+                                if "-n" in flags:
+                                    non_matching_lines += file + ":" + str(line_counter) + ":" + line
+                                else:
+                                    non_matching_lines += file + ":" + line
+                        line_counter += 1
+                except:
+                    logging.error(f"Provided file {file} does not exist")
+        else:
+            return ""
 
-    if "-l" not in flags and "-v" not in flags:
-        return result
-    elif "-v" in flags:
-        return non_matching_lines
-    elif "-l" in flags:
-        return return_filenames(files, result)
+        if "-l" not in flags and "-v" not in flags:
+            return result
+        # if -v: collect all lines that fail to match the pattern
+        elif "-v" in flags:
+            return non_matching_lines
+        elif "-l" in flags:
+            return return_filenames(first_file, result, len(files))
+    except:
+        logging.error("Invalid input")
 
 
 def get_matched_one_file(line_counter, line, pattern, result, flags, non_matching_lines=""):
     line_to_match = line
     if "-i" in flags:
         line_to_match = line.lower()
+    # if -x in flags - only match entire lines, instead of lines that contain a match
     if "-x" in flags:
         if pattern + "\n" == line_to_match and "-n" in flags:
             result += str(line_counter) + ":" + line
         elif pattern + "\n" == line_to_match:
             result += line
+        # if -v: collect all lines that fail to match the pattern
         elif "-v" in flags:
             non_matching_lines += line
     else:
@@ -67,6 +90,7 @@ def get_matched_one_file(line_counter, line, pattern, result, flags, non_matchin
             result += str(line_counter) + ":" + line
         elif pattern in line_to_match:
             result += line
+        # if -v: collect all lines that fail to match the pattern
         elif "-v" in flags:
             if "-n" in flags:
                 non_matching_lines += str(line_counter) + ":" + line
@@ -81,23 +105,16 @@ def get_data_from_file(file_name):
     return datafile
 
 
-def return_filenames(files, result):
+def return_filenames(first_file, result, files_number):
     if not result:
         return ""
-    elif len(files) == 1:
-        return files[0] + "\n"
+    elif files_number == 1:
+        return first_file + "\n"
     else:
         files_as_result = ""
         result_as_list = result.split("\n")
         for line in result_as_list:
-            if line.split(":")[0] not in files_as_result:
-                files_as_result += line.split(":")[0] + "\n"
+            first_filename = line.split(":")[0]
+            if first_filename not in files_as_result:
+                files_as_result += first_filename + "\n"
         return files_as_result
-
-
-def main():
-    logging.info(grep("Forbidden", "-l", ["paradise-lost.txt"]))  # "paradise-lost.txt\n"
-
-
-if __name__ == "__main__":
-    main()
