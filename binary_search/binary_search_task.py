cbc9834bee4e5679fb69e5b8190964de81f174ff
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-09 13:24:23 +0200

    Removing folder that was added by mistake

--- binary_search/binary_search_task.py
+++
@@ -1,7 +0,0 @@
-# Task from https://exercism.org/tracks/python/exercises/binary-search
-
-def find(search_list, value):
-    try:
-        return search_list.index(value)
-    except ValueError:
-        raise ValueError("value not in array")
