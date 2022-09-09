7608a4802f6aac7382cf65713ed81c0a1b5fdb70
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-09-09 16:28:30 +0200

    Re-wrote solution from the scratch

--- pig_latin/pig_latin_task.py
+++ pig_latin/pig_latin_task.py
@@ -1,38 +1,49 @@
 # Task from https://exercism.org/tracks/python/exercises/pig-latin
+import logging
 
-consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
+logging.basicConfig(level=logging.INFO)
+
+VOWEL = ("xr", "yt", "a", "e", "i", "o", "u")
+CONSONANTS = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z")
 
 
-def translate(text):
-    if type(text) == str and " " not in text:
-        return translate_single_word(text)
-    else:
-        translated_phrase = ""
+def translate(text: str):
+    try:
+        text = text.lower()
+        result = ""
         for word in text.split(" "):
-            translated_phrase += translate_single_word(word) + " "
-        return translated_phrase[:-1]
+            result += translate_single_word(word) + " "
+        # return result without extra space
+        return result[:-1]
+    except:
+        logging.error("Invalid input")
 
 
-def translate_single_word(text):
-    if text.startswith("xr") or text.startswith("yt"):
-        return text + "ay"
-    elif text.startswith("qu"):
-        return text[2:] + "quay"
-    elif text.startswith(tuple(consonants)):
-        return translate_starting_with_consonants(text)
-    else:
-        if not translate_starting_with_consonants(text):
-            return text + "ay"
-        else:
-            return translate_starting_with_consonants(text)
+def translate_single_word(word):
+    # for rule 1
+    if word.startswith(VOWEL):
+        return word + "ay"
+    # for rules 2, 3, 4
+    elif word.startswith(CONSONANTS):
+        # need to check if there's just one consonant or more; and if it's followed by qu; or followed by y
+        return starts_with_consonants(word)
+    return word
 
 
-def translate_starting_with_consonants(text):
-    translated_text = text
-    for letter in text:
-        if translated_text.startswith("qu"):
-            return translated_text[2:] + "quay"
-        elif translated_text[0] in consonants or (text[0] is "y" and translated_text[0] == text[0]):
-            translated_text = translated_text[1:] + letter
-        else:
-            return translated_text + "ay"
+def starts_with_consonants(word: str):
+    result = word
+    for letter in word:
+        if result.startswith("qu"):
+            result = result[2:] + result[:2]
+        elif result.startswith(CONSONANTS) or letter is word[0]:
+            result = result[1:] + result[0]
+    return result + "ay"
+
+
+def main():
+    for word in ["xray", "yttria", "chair", "square", "rhythm", "my", "xray my xray"]:
+        logging.info(translate(word))
+
+
+if __name__ == "__main__":
+    main()
